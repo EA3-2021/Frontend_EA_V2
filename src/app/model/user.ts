a3068e75eb261e589cd8efeb9e934e9e1ed3539e
sumayabl <>
2021-06-10 14:52:19 +0200

    Register user on process 3

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,7 +1,10 @@
 export interface User {
+    company: string;
     name: string;
     email: string;
     phone: string;
     password: string;
     token: string;
+    workerID: string;
+    petition: boolean;
 }
--- src/app/pages/GeneralUser/register-user/register-user.page.ts
+++ src/app/pages/GeneralUser/register-user/register-user.page.ts
@@ -12,30 +12,46 @@ import { UserService } from '../../../services/user.service';
 })
 export class RegisterUserPage implements OnInit {
 
-  registerAdminForm: FormGroup;
+  registerUserForm: FormGroup;
+  submitted = false;
 
   constructor(
     private formBuilder: FormBuilder,
       private route: ActivatedRoute,
       private router: Router,
-      private adminService: UserService
+      private userService: UserService
   ) { }
 
   ngOnInit() {
-    this.registerAdminForm = this.formBuilder.group({
+    this.registerUserForm = this.formBuilder.group({
     name: ['', Validators.required],
-    cif: ['', Validators.required],
     email: ['', Validators.required],
     phone: ['', Validators.required],
-    address: ['', Validators.required],
-    postalCode: ['', Validators.required],
     password: ['', Validators.required]
   });
   }
 
   ionChanger(){
-   this.registerAdminForm.enable();
+   this.registerUserForm.enable();
   }
 
+  // convenience getter for easy access to form fields
+  get formControls() { return this.registerUserForm.controls; }
+
+  submitRegisterUser() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerUserForm.invalid) {
+          return;
+      }
+
+      this.userService.registerUser(this.registerUserForm.value)
+            .pipe(first())
+            .subscribe(() => {
+              //Llevarte una page anterior con tostada avisando de register exitoso
+                    this.router.navigate(['/login-user']);
+                });
+  }
 
 }
